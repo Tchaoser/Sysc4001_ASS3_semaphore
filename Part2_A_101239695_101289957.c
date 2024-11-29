ad000c7b157dff3cbff274e76988b606a7551e8d
Tchaoser <sapthakeerthi.u@gmail.com>
2024-11-29 11:13:26 -0500

    exoerimetntall code

--- Part2_A_101239695_101289957.c
+++ Part2_A_101239695_101289957.c
@@ -10,6 +10,15 @@ int TA() {
 
 
 int main(int argc, char* argv[]) {
+    int pid = fork();
+
+    if (pid == 0) {
+        printf("I am the child process!\n");
+    } else {
+        printf("I am the parent process!\n");
+    }
+
+    return 0;
 
 }
 
