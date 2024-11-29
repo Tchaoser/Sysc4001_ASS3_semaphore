04725c5eccc5dddd32841fea6a01dc329d8ce24b
Tchaoser <113814746+Tchaoser@users.noreply.github.com>
2024-11-29 11:20:56 -0500

    Update Part2_A_101239695_101289957.c

--- Part2_A_101239695_101289957.c
+++ Part2_A_101239695_101289957.c
@@ -1,5 +1,7 @@
 
 
+#include <stdio.h>
+#include <unistd.h>  // Required for fork()
 
 
 int TA() {
@@ -8,24 +10,36 @@ int TA() {
 }
 
 
-
+//5 processes base code
 int main(int argc, char* argv[]) {
-    int pid0 = fork();
-    int pid1 = fork();
-    int pid2 = fork();
-    int pid3 = fork();
-    int pid4 = fork();
-
-
+    // Create 4 child processes in total
+    int pid0 = fork();  // First fork
     if (pid0 == 0) {
-        printf("I am the child process 0!\n");
-    } else if (pid1 == 1) {
-        printf("child 1")
+        // This is the first child process
+        printf("I am childc process 1! My PID is %d, Parent PID is %d\n", getpid(), getppid());
     } else {
-        printf("I am the parent process!\n");
+        int pid1 = fork();  // Second fork
+        if (pid1 == 0) {
+            // This is the second child process
+            printf("I am child process 2! My PID is %d, Parent PID is %d\n", getpid(), getppid());
+        } else {
+            int pid2 = fork();  // Third fork
+            if (pid2 == 0) {
+                // This is the third child process
+                printf("I am child process 3! My PID is %d, Parent PID is %d\n", getpid(), getppid());
+            } else {
+                int pid3 = fork();  // Fourth fork
+                if (pid3 == 0) {
+                    // This is the fourth child process
+                    printf("I am child process 4! My PID is %d, Parent PID is %d\n", getpid(), getppid());
+                } else {
+                    // This is the parent process, which has created 4 child processes
+                    printf("I am the parent process! My PID is %d\n", getpid());
+                }
+            }
+        }
     }
 
     return 0;
-
 }
 
